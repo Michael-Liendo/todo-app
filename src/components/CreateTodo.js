c684f50647259144ba480db21caa38cbec3cdd49
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-07-01 14:01:56 -0400

    feat: change the arrow and quit outline of the input

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -4,9 +4,12 @@ import arrow from '../assets/Arrow.svg';
 export default function CreateTodo() {
   return (
     <div className="flex">
-      <input className="bg-[#25273C] p-4 w-96 h-14 rounded-l-3xl" />
-      <button className="bg-[#5aff31cb] w-14 h-14 rounded-r-3xl">
-        <Image className="fixed" src={arrow} alt="Arrow curve" width={40} />
+      <input
+        placeholder="Create a new ToDo..."
+        className="bg-[#25273C] focus:outline-none border-dashed border-2 border-sky-500 text-[#ffffffb9] p-4 w-96 h-14 rounded-l-3xl"
+      />
+      <button className="transition duration-500 hover:scale-110 bg-[#5aff31cb] w-14 h-14 rounded-r-3xl">
+        <Image className="fixed" src={arrow} alt="Arrow curve" height={40} />
       </button>
     </div>
   );
