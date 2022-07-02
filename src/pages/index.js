ea22c707f681739c20beee878d8d6b856fe2b29e
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-07-02 12:11:32 -0400

    fix: letter spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import CreateTodo from '../components/CreateTodo';
 export default function Home() {
   return (
     <>
-      <h1 className="text-6xl text-left m-7">TODO</h1>
+      <h1 className="text-6xl text-left m-7 tracking-widest">TODO</h1>
 
       <div>
         <CreateTodo />
