53502a66846b55fe18067a00b9179997cf0a407f
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-07-07 21:08:45 -0400

    feat: delete task

--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { useEffect } from 'react';
 import Check from '../assets/Check.svg';
 import Line from '../assets/Line.svg';
+import Trash from '../assets/Trash.svg';
 
 export default function ListTodo({ toDos, setToDos }) {
   useEffect(() => {
@@ -16,6 +17,13 @@ export default function ListTodo({ toDos, setToDos }) {
     setToDos(items);
   }
 
+  function deleteTodo(index) {
+    let items = JSON.parse(localStorage.getItem('items'));
+    items.splice(index, 1);
+    localStorage.setItem('items', JSON.stringify(items));
+    setToDos(items);
+  }
+
   return (
     <div className="flex justify-center mt-16">
       <div className="bg-[#25273C] w-full p-4 rounded-md sm:w-1/2">
@@ -23,26 +31,46 @@ export default function ListTodo({ toDos, setToDos }) {
           return (
             <div key={index}>
               {todo.completed ? (
-                <div className="flex my-4">
-                  <button
-                    onClick={() => changeStatusTask(index)}
-                    className="mr-2.5 bg-gradient-to-r from-[#E600FA] rounded-full px-1 pt-1"
-                  >
-                    <Image src={Check} alt="Arrow curve" />
-                  </button>
-                  <p className="ml-2.5">
-                    <s>{todo.name}</s>
-                  </p>
+                <div className="flex justify-between my-4">
+                  <div className="flex">
+                    <button
+                      onClick={() => changeStatusTask(index)}
+                      className="mr-2.5 bg-gradient-to-r from-[#E600FA] rounded-full px-1 pt-1"
+                    >
+                      <Image src={Check} alt="Check" />
+                    </button>
+                    <p className="ml-2.5">
+                      <s>{todo.name}</s>
+                    </p>
+                  </div>
+                  <div>
+                    <Image
+                      src={Trash}
+                      alt="trash"
+                      className="cursor-pointer"
+                      onClick={() => deleteTodo(index)}
+                    />
+                  </div>
                 </div>
               ) : (
-                <div className="flex my-4">
-                  <button
-                    onClick={() => changeStatusTask(index)}
-                    className="mr-2.5 grid place-items-center bg-slate-500 rounded-full px-1 py-3"
-                  >
-                    <Image src={Line} width={18} alt="Arrow curve" />
-                  </button>
-                  <p className="ml-2.5">{todo.name}</p>
+                <div className="flex justify-between my-4">
+                  <div className="flex">
+                    <button
+                      onClick={() => changeStatusTask(index)}
+                      className="mr-2.5 grid place-items-center bg-slate-500 rounded-full px-1 py-3"
+                    >
+                      <Image src={Line} width={18} alt="Line" />
+                    </button>
+                    <p className="ml-2.5">{todo.name}</p>
+                  </div>
+                  <div>
+                    <Image
+                      src={Trash}
+                      alt="trash"
+                      className="cursor-pointer"
+                      onClick={() => deleteTodo(index)}
+                    />
+                  </div>
                 </div>
               )}
             </div>
